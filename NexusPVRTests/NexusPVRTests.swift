88f666693ae2685531e6d5a1ca5ddd5a15a0ae53
drvolks <5374619+drvolks@users.noreply.github.com>
2026-02-02 16:50:57 -0500

    first commit

---
+++ NexusPVRTests/NexusPVRTests.swift
@@ -0,0 +1,16 @@
+//
+//  NexusPVRTests.swift
+//  NexusPVRTests
+//
+//  Created by drvolks on 2026-02-02.
+//
+
+import Testing
+
+struct NexusPVRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
